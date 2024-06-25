7f4d5490208df182b18bec641f0fa792cf94eb7f
MKravczak <krawczak.mikolaj@gmail.com>
2024-06-25 23:55:36 +0200

    Adding first buttons

--- Model.py
+++ Model.py
@@ -1,18 +1,35 @@
 import Luggage
 import random
 import numpy
+import customtkinter as CTK
 from random import choices
+
+
 
 names = ["John", "Jane", "Jack", "Jill", "James", "Jenny", "Jasper", "Jasmine", "Jared", "Jade"]
 surnames = ["Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor"]
 types= ["suitcase","hiking bag","backpack"]
-departures = ["New York", "Lodon", "Paris", "Berlin", "Tokyo", "Beijing", "Moscow", "Sydney", "Cairo", "Rio de Janeiro"]
+departures = ["New York", "Lodon", "Paris", "Berlin", "Tokyo", "Beijing"]
 baggages = []
 
-for i in range(1,11):
-    baggage = Luggage.Luggage(i,random.choice(names),random.choice(surnames),round(random.uniform(8,20),2), random.choice(departures), str((numpy.random.choice(types, 1, p=[0.7, 0.2, 0.1])[0])))
-    print(baggage)
-    baggages.append(baggage)
+def LuggageCreate():
+    baggages.clear()
+    for i in range(1, 11):
+        baggage = Luggage.Luggage(i, random.choice(names), random.choice(surnames), round(random.uniform(8, 20), 2),
+                                  random.choice(departures), str((numpy.random.choice(types, 1, p=[0.7, 0.2, 0.1])[0])))
+        print(baggage)
+        baggages.append(baggage)
 
+def clear_luggage_list_div(luggage_list_div):
+    for widget in luggage_list_div.winfo_children():
+        widget.destroy()
 
+def LuggageList(luggage_list_div):
+    clear_luggage_list_div(luggage_list_div)
+    for i in baggages:
+        listbox = CTK.CTkTextbox(master=luggage_list_div, width=400, height=20, fg_color="black", border_color="#17AB99", activate_scrollbars=False, border_width=1)
+        listbox.pack(pady=0)
+        listbox.delete("1.0", "end")
+        listbox.insert("end", f" ID:  {i.luggage_id}          Type:    {i.type}           \n")
+        listbox.configure(state='disabled')
 
--- UI_menu.py
+++ UI_menu.py
@@ -1,4 +1,5 @@
 import customtkinter as CTK
+
 import Model
 import tkinter as TK
 
@@ -7,7 +8,7 @@ CTK.set_default_color_theme("dark-blue")
 
 screen = CTK.CTk()
 screen.title("Air Port Luggage System")
-screen.geometry("1200x900")
+screen.geometry("1600x900")
 
 frame = CTK.CTkFrame(master=screen, fg_color="black", bg_color="aqua",border_color="#17AB99", border_width=1)
 frame.pack(pady=50, padx=50, fill="both", expand=True)
@@ -16,26 +17,55 @@ title = CTK.CTkLabel(master=frame, text="Air Port Luggage System", font=CTK.CTkF
 title.pack(pady=15)
 
 
-luggage_list_div = CTK.CTkFrame(master=frame, width=400,  fg_color="black", bg_color="aqua",border_color="#17AB99", border_width=1)
-luggage_list_div.pack(pady=20, padx=20,side="left", fill="both")
-luggage_list_title = CTK.CTkLabel(master=luggage_list_div, width=400, text="Luggage's list", font=CTK.CTkFont(family="terminal", size=20, weight="bold"))
+luggage_list_divA = CTK.CTkFrame(master=frame, width=400,  fg_color="black", bg_color="aqua",border_color="#17AB99", border_width=1)
+luggage_list_divA.pack(pady=20, padx=20,side="left", fill="both")
+luggage_list_title = CTK.CTkLabel(master=luggage_list_divA, width=200, text="Luggage's list", font=CTK.CTkFont(family="terminal", size=20, weight="bold"))
 luggage_list_title.pack(pady=25)
+luggage_list_div = CTK.CTkFrame(master=luggage_list_divA, width=400,  fg_color="black", border_color="#17AB99", border_width=0)
+luggage_list_div.pack(pady=20, padx=20,side="left", fill="both")
 
 
 
-for i in Model.baggages:
-    listbox = CTK.CTkTextbox(master=luggage_list_div, width=400, height=20, fg_color="black", border_color="#17AB99", activate_scrollbars=False, border_width=1)
-    listbox.pack(pady=0)
-    listbox.insert("end", f" ID:  {i.luggage_id}          Type:    {i.type}           \n")
-    listbox.configure(state='disabled')
 
 
 
-work_zone = CTK.CTkFrame(master=frame, width=800,  fg_color="black", border_color="#17AB99", border_width=1)
-work_zone.pack(pady=20, padx=20,side="left", fill="both")
-work_zone_title = CTK.CTkLabel(master=work_zone, width=800, text="Control panel", font=CTK.CTkFont(family="terminal", size=20, weight="bold"))
+
+work_zoneA = CTK.CTkFrame(master=frame, width=800,  fg_color="black", border_color="#17AB99", border_width=1)
+work_zoneA.pack(pady=20, padx=20,side="left", fill="both")
+
+work_zone_title = CTK.CTkLabel(master=work_zoneA, width=800, text="Control panel", font=CTK.CTkFont(family="terminal", size=20, weight="bold"))
 work_zone_title.pack(pady=25)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+work_zone = CTK.CTkFrame(master=work_zoneA, width=200,  fg_color="black", border_color="#17AB99", border_width=1)
+work_zone.pack(pady=20, padx=20,side="left", fill="both")
 
+button = CTK.CTkButton(master=work_zone, width=150, height= 40, corner_radius=3,  text_color="Black",fg_color="#17AB99", hover_color="#84AAAF", text="Generate Luggage List", command=lambda: Model.LuggageCreate())
+button.pack(pady=10, padx=50, side="top", anchor="nw")
+
+button = CTK.CTkButton(master=work_zone, width=150, height= 40, corner_radius=3,  text_color="Black",fg_color="#17AB99", hover_color="#84AAAF", text="Show Luggage List", command=lambda: Model.LuggageList(luggage_list_div))
+button.pack(pady=10, padx=50, side="top", anchor="nw")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+work_zoneB = CTK.CTkFrame(master=work_zoneA, width=400, height=400,  fg_color="black", border_color="#17AB99", border_width=1)
+work_zoneB.pack(pady=20, padx=20,side="top", fill="both")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+work_zoneC = CTK.CTkFrame(master=work_zoneA, width=400, height=350 ,  fg_color="black", border_color="#17AB99", border_width=1)
+work_zoneC.pack(pady=20, padx=20,side="top", fill="both")
+
+luggage_input = CTK.CTkEntry(master=work_zoneC, width=40, font=CTK.CTkFont(family="terminal", size=20, weight="bold"), height= 40, corner_radius=3,  text_color="Black",fg_color="#17AB99")
+luggage_input.pack(pady=10, padx=50, side="top", anchor="nw")
+
+
+
+
+
+
+departures_div = CTK.CTkFrame(master=frame, width=500,  fg_color="black", bg_color="aqua",border_color="#17AB99", border_width=1)
+departures_div.pack(pady=20, padx=20,side="left", fill="both")
+
+
+departures_div_title = CTK.CTkLabel(master=departures_div, width=500, text="Departures", font=CTK.CTkFont(family="terminal", size=20, weight="bold"))
+departures_div_title.pack(pady=25)
 
 
 
